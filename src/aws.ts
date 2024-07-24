dc13562a0063b847b5a3a3e98a4f46f649e95d74
Moutaz Ali <moutaz.ali.dev@gmail.com>
2024-07-24 12:29:54 +0300

    migrating to aws-sdk-v3, create,describe,delete table func,create putItem func, seed all data into dynamodb db

--- src/aws.ts
+++ src/aws.ts
@@ -1,18 +1,67 @@
-import AWS from "aws-sdk";
+import {
+  DynamoDBClient,
+  CreateTableCommand,
+  DescribeTableCommand,
+  DeleteTableCommand,
+  PutItemCommand,
+  CreateTableCommandInput,
+  DescribeTableCommandOutput,
+  DeleteTableCommandOutput,
+  PutItemCommandOutput,
+} from "@aws-sdk/client-dynamodb";
+import { marshall } from "@aws-sdk/util-dynamodb";
 import { AWSRegions } from "./types/aws.js";
+import { Vendor } from "./types/twitter.js";
 
-AWS.config.update({ region: AWSRegions.US_EAST_1 });
-
-const { DynamoDB } = AWS;
-
-const dynamoDB = new DynamoDB();
+// Initialize the DynamoDB client
+const dynamoDB = new DynamoDBClient({ region: AWSRegions.US_EAST_1 });
 
 export const dynamodbCreateTable = async (
-  params: AWS.DynamoDB.CreateTableInput
-) => {
+  params: CreateTableCommandInput
+): Promise<DescribeTableCommandOutput> => {
   try {
-    const result = await dynamoDB.createTable(params).promise();
-    console.log(result);
+    const command = new CreateTableCommand(params);
+    const result = await dynamoDB.send(command);
+    return result;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const dynamodbDescribeTable = async (
+  tableName: string
+): Promise<DescribeTableCommandOutput> => {
+  try {
+    const command = new DescribeTableCommand({ TableName: tableName });
+    const result = await dynamoDB.send(command);
+    return result;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const dynamodbDeleteTable = async (
+  tableName: string
+): Promise<DeleteTableCommandOutput> => {
+  try {
+    const command = new DeleteTableCommand({ TableName: tableName });
+    const result = await dynamoDB.send(command);
+    return result;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const dynamodbCreateRecord = async (
+  tableName: string,
+  vendor: Vendor
+): Promise<PutItemCommandOutput> => {
+  try {
+    const command = new PutItemCommand({
+      TableName: tableName,
+      Item: marshall(vendor) as any,
+    });
+    const result = await dynamoDB.send(command);
     return result;
   } catch (error) {
     throw error;
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,20 @@
 import AWS from "aws-sdk";
-import { dynamodbCreateTable } from "./aws.js";
+import {
+  dynamodbCreateRecord,
+  dynamodbCreateTable,
+  dynamodbDeleteTable,
+  dynamodbDescribeTable,
+} from "./aws.js";
+import { CreateTableCommandInput } from "@aws-sdk/client-dynamodb";
+import vendors from "./data/vendors.js";
 
-const vendorsTableParams: AWS.DynamoDB.CreateTableInput = {
+const delay = (ms: number) => {
+  return new Promise((res, rej) => {
+    setTimeout(res, ms);
+  });
+};
+
+const vendorsTableParams: CreateTableCommandInput = {
   TableName: "vendors",
   KeySchema: [{ AttributeName: "twitterId", KeyType: "HASH" }],
   AttributeDefinitions: [
@@ -11,9 +24,28 @@ const vendorsTableParams: AWS.DynamoDB.CreateTableInput = {
     },
   ],
   ProvisionedThroughput: {
-    ReadCapacityUnits: 10,
-    WriteCapacityUnits: 10,
+    ReadCapacityUnits: 5,
+    WriteCapacityUnits: 5,
   },
 };
 
-dynamodbCreateTable(vendorsTableParams);
+const init = async () => {
+  const tableName = "vendors";
+  const vendorsTable = await dynamodbDescribeTable(tableName);
+  if (!(vendorsTable instanceof Error)) {
+    await dynamodbDeleteTable(tableName);
+    await delay(6000);
+  }
+  await dynamodbCreateTable(vendorsTableParams);
+  await delay(6000);
+  for (const i in vendors) {
+    const vendor = vendors[i];
+    const record = await dynamodbCreateRecord(tableName, vendor);
+    if (record instanceof Error) {
+      console.log(record, vendor);
+    }
+    console.log("record created");
+  }
+};
+
+init();
